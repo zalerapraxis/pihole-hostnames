bfd9151df9339dc9cb70e204c0ee343e0cf8be91
Zalera <cjpff14@gmail.com>
2020-06-11 15:43:09 -0400

    use nmap scanning instead of dhcp sniffing

--- discovery.py
+++ discovery.py
@@ -18,107 +18,62 @@ from scapy.all import *
 from python_hosts import Hosts, HostsEntry
 from shutil import copyfile
 import sys
+import filecmp
+import nmap
 
 
 '''
 Global stuff
 '''
-
-interface = "eth0"
-addressList =  {
-  "70:85:c2:a1:ba:3f": "Dalamud",
-  "20:39:56:dd:a6:a9": "Android-Nokia",
-  "98:ee:cb:3a:9e:84": "Acer",
-  "b0:c1:9e:fd:1c:b4": "Android-ZTE",
-  "70:f1:a1:74:ad:97": "HP",
-  "ac:ae:19:0c:11:6f": "Roku-Downstairs",
-  "b8:3e:59:81:4e:4f": "Roku-Upstairs",
-  "00:f6:20:45:6f:77": "Google-Home",
-  "14:10:9f:03:22:dd": "iPod"
+addressList ={
+    "E8:6F:F2:18:2E:B0": "Router",
+    "70:85:C2:A1:BA:3F": "Dalamud",
+    "20:39:56:DD:A6:A9": "Android-Nokia",
+    "98:EE:CB:3A:9E:84": "Acer",
+    "B0:C1:9E:FD:1C:B4": "Android-ZTE",
+    "70:F1:A1:74:AD:97": "HP",
+    "AC:AE:19:0C:11:6F": "Roku-Downstairs",
+    "B8:3E:59:81:4E:4F": "Roku-Upstairs",
+    "00:F6:20:45:6F:77": "Google-Home",
+    "14:10:9F:03:22:DD": "iPod",
+    "00:15:5D:F2:81:1F": "VM-XIVAPI",
+    # "E8:6F:F2:18:2E:B0" is the pihole, it's registered as localhost
 }
 
-'''
-Log message for troubleshooting
-'''
 
-def log_packet_info(packet):
-    #print(packet.summary())
-    #print(ls(packet))
-    print('---')
-    types = {
-        1: "New DHCP Discover",
-        2: "New DHCP Offer",
-        3: "New DHCP Request",
-        5: "New DHCP Ack",
-        8: "New DHCP Inform"
-    }
-    if DHCP in packet:
-        print(types.get(packet[DHCP].options[0][1], "Some Other DHCP Packet"))
-    return
+''' main function '''
 
+print("Starting...\n")
 
-# https://jcutrer.com/howto/dev/python/python-scapy-dhcp-packets
-def get_option(dhcp_options, key):
-    must_decode = ['hostname', 'domain', 'vendor_class_id']
-    try:
-        for i in dhcp_options:
-            if i[0] == key:
-                # If DHCP Server Returned multiple name servers 
-                # return all as comma seperated string.
-                if key == 'name_server' and len(i) > 2:
-                    return ",".join(i[1:])
-                # domain and hostname are binary strings,
-                # decode to unicode string before returning
-                elif key in must_decode:
-                    return i[1].decode()
-                else: 
-                    return i[1]        
-    except:
-        pass
-
-def handle_dhcp_packet(packet):
-    log_packet_info(packet)
-    if DHCP in packet:
-        requested_addr = get_option(packet[DHCP].options, 'requested_addr')
-        hostname = get_option(packet[DHCP].options, 'hostname')
-        param_req_list = get_option(packet[DHCP].options, 'param_req_list')
-        vendor_class_id = get_option(packet[DHCP].options, 'vendor_class_id')
-        print(f"Host {hostname} ({packet[Ether].src}) requested {requested_addr}.")
-        if (requested_addr):
-            update_hosts_file(requested_addr, hostname, packet[Ether].src)
-    return
-
-'''
-Update the hosts file with specified name for hostname
-If host's mac address is not present in our address list, use 
-'''
-
-def update_hosts_file(address,hostname,macaddr):
-    if macaddr in addressList:
-        etchostname = addressList[macaddr]
-    elif (hostname):
-        print (f"Host {hostname} ({macaddr}) was not found in list")
-        etchostname = hostname
-
+while True:
     copyfile("/etc/hosts", "hosts")
-    print(f"Updating hostname as: {etchostname} with {address}")
-
     hosts = Hosts(path='hosts')
-    hosts.remove_all_matching(name=etchostname)
-    new_entry = HostsEntry(entry_type='ipv4', address=address, names=[etchostname])
-    hosts.add([new_entry])
+
+    nm = nmap.PortScanner()
+    nm.scan('192.168.254.0/24', arguments='-sn')
+    for host in nm.all_hosts():
+        if 'mac' in nm[host]['addresses']:
+            ipaddress = nm[host]['addresses']['ipv4']
+            macaddress = nm[host]['addresses']['mac']
+
+            if macaddress in addressList:
+                etchostname = addressList[macaddress]
+            else:
+                print(f"Device at {ipaddress} ({macaddress}) isn't in our list.")
+                break
+
+            if not hosts.exists(ipaddress, etchostname):
+                print(f"Updating hostname: {etchostname} with {ipaddress}")
+                hosts.remove_all_matching(name=etchostname)
+                new_entry = HostsEntry(entry_type='ipv4', address=ipaddress, names=[etchostname])
+                hosts.add([new_entry])
+
     hosts.write()
-    copyfile("hosts", "/etc/hosts")
 
-    print(f"Updated Host name for hosts file is {etchostname}")
+    if not filecmp.cmp("hosts", "/etc/hosts", shallow=False):
+        print("Changes detected, writing new hosts file")
+        copyfile("hosts", "/etc/hosts")
 
-    return
+        print("==========================================")
 
-            
-print("Starting\n")
-sniff(iface = interface, filter='udp and (port 67 or 68)', prn = handle_dhcp_packet, store = 0)
-print("\n Shutting down...")
-
-'''
-End of file
-'''
+    time.sleep(30)
